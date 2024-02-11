333c987ba93b103ab757de0f8ddf084be2380930
pratham-aggarwal21 <157262435+pratham-aggarwal21@users.noreply.github.com>
2024-02-10 18:44:05 -0800

    Add files via upload

---
+++ Nested Loops/nestedloop.py
@@ -0,0 +1,181 @@
+### Seq1
+##for i in range(4):
+##    for j in range(1, 5):
+##        print(j, end='')
+##    print()
+
+
+### Seq2
+##for i in range(4):
+##    for j in range(4, 0, -1):
+##        print(j, end='')
+##    print()
+##
+### Seq3
+##for i in range(1, 5):
+##    for j in range(4):
+##        print(i, end='')
+##    print()
+##
+### Seq4
+##for i in range(3, 0, -1):
+##    for j in range(1, 5):
+##        print(i, end='')
+##    print()
+##
+### Seq5
+##for i in range(4):
+##    for j in range(0, 8, 2):
+##        print(j, end='')
+##    print()
+##
+### Seq6
+##for i in range(4):
+##    for j in range(6, -1, -2):
+##        print(j, end='')
+##    print()
+##
+### Seq7
+##for i in range(1, 8, 2):
+##    for j in range(4):
+##        print(i, end='')
+##    print()
+##
+### Seq8
+##for i in range(7, 0, -2):
+##    for j in range(4):
+##        print(i, end='')
+##    print()
+##
+### Seq9
+##for i in range(7, 0, -2):
+##    for j in range(1, 5):
+##        print(i, end='')
+##    print()
+##
+
+### Seq10
+for i in range(4, 0, -1):
+   for j in range(i, 0, -1):
+       print(j, end='')
+   print()
+
+### Seq11
+##for i in range(4):
+##    for j in range(65, 69):
+##        print(chr(j), end='')
+##    print()
+##
+### Seq12
+##for i in range(65, 69):
+##    for j in range(i, i + 8, 2):
+##        print(chr(j), end='')
+##    print()
+##
+### Seq13
+##for i in range(69, 65, -1):
+##    for j in range(i, i - 8, -2):
+##        print(chr(j), end='')
+##    print()
+##
+### Seq14
+##for i in range(65, 69):
+##    for j in range(4):
+##        print(chr(i), end='')
+##    print()
+##
+### Seq15
+##for i in range(68, 64, -1):
+##    for j in range(4):
+##        print(chr(i), end='')
+##    print()
+##
+### Seq16
+##for i in range(68, 64, -1):
+##    for j in range(i, i - 8, -2):
+##        print(chr(j), end='')
+##    print()
+##
+### Seq17
+##num = 1
+##for i in range(3):
+##    for j in range(4):
+##        print(num, end='')
+##        num += 1
+##    print()
+##
+### Seq18
+##num = 1
+##for i in range(5):
+##    for j in range(1, i + 2):
+##        print(num, end='')
+##        num += 1
+##    print()
+##
+### Seq19
+##for i in range(1, 5):
+##    for j in range(i):
+##        print(1, end='')
+##    print()
+##
+### Seq20
+##num = 1
+##for i in range(4):
+##    for j in range(i + 1):
+##        print(num, end='')
+##        num += 1
+##    print()
+##
+### Seq21
+##for i in range(4):
+##    for j in range(i + 1):
+##        print(i + 1, end='')
+##    print()
+##
+### Seq22
+##for i in range(4):
+##    for j in range(i + 1):
+##        print(j + 1, end='')
+##    print()
+##
+### Seq23
+##for i in range(4):
+##    for j in range(i + 1):
+##        print(2 * j + 1, end='')
+##    print()
+##
+### Seq24
+##for i in range(4):
+##    for j in range(i + 1):
+##        print(2 ** j, end='')
+##    print()
+##
+### Seq25
+##for i in range(4):
+##    for j in range(i + 1):
+##        print(8 - 2 * j, end='')
+##    print()
+##
+### Seq26
+##for i in range(4):
+##    for j in range(i + 1):
+##        print(chr(65 + j), end='')
+##    print()
+##
+### Seq27
+##for i in range(4):
+##    for j in range(i + 1):
+##        print(chr(68 - j), end='')
+##    print()
+##
+### Seq28
+##for i in range(4):
+##    for j in range(i + 1):
+##        print(chr(65 + i), end='')
+    ##print()
+
+# Seq29
+##for i in range(4):
+##    for j in range(i + 1):
+##        print(chr(68 - i), end='')
+##    print()
