413b10b7178399b338443094e164bd2508fb98ff
pratham-aggarwal21 <157262435+pratham-aggarwal21@users.noreply.github.com>
2024-03-04 09:27:34 -0800

    Add files via upload

---
+++ Library/LibrComputerSubjects/ComputerScience/CSXI.py
@@ -0,0 +1,16 @@
+#CSXI.py
+"""Details about CBSE CS XI"""
+def Syllabus():
+    print("Unit 1: Programming and Computtational Thinking-1") 
+    print(" :Pythom basics, basic sorting techniques etc. ") 
+    print("Unit 2 : Computer Systems and Organisation ") 
+    print(" :Computer organisation, execution, cloud computing etc. ") 
+    print("Unit 3 : Data Management 1") 
+    print(": SQL, basics of NoSQL databases etc. ") 
+    print("Unit 4 Society, Law and ethics 1") 
+    print(" :Cyber Safety, safe data communication etc. ") 
+    print("Unit 5 Practical Unit")
+
+def About():
+    print("Computer Science XI") 
+    print("Contains 4 units and a Practical unit") 
