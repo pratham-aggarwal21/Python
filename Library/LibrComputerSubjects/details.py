ebe5eb93f358dc6270840cb3e616f05bec861550
pratham-aggarwal21 <157262435+pratham-aggarwal21@users.noreply.github.com>
2024-03-04 09:26:59 -0800

    Add files via upload

---
+++ Library/LibrComputerSubjects/details.py
@@ -0,0 +1,6 @@
+#details.py
+
+""" Details about Computer Sicence subjects in CBSE """
+def SubjectsList():
+    print("There are two computer science subjects in CBSE")
+    print("'Computer Scinece' and 'Informatics Practices'")
